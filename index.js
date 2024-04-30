224afd0f655571aee378da81898f57541ac7372e
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-04-30 19:41:22 +0600

    planets name displayed

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ fs.createReadStream('kepler_data.csv')  //here we are streaming the data from cs
         console.log(err);
     })
     .on('end', ()=>{
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }));
+
         console.log(`${habitablePlanets.length} habitable planets found!`);
         console.log('done!');
     });
