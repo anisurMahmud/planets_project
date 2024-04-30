9f5252f89d7cc499e77e3c2a347c7546feda6084
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-04-30 16:28:28 +0600

    reading files using stream by fs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const parse = require('csv-parse');
+const fs = require('fs');   //this is filesystem module to be used to read the csv file
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')  //here we are streaming the data from csv file
+.on('data', (data) => {     //here on data 
+    results.push(data);
+    })
+.on('error', (err)=>{
+    console.log(err);
+})
+.on('end', ()=>{
+    console.log(results);
+    console.log('done');
+});
